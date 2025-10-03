6a7032006777cbc227fbf21e5e93afb6a6a95c09
Devon Riebe-Rutlin <leeks.shaken11@icloud.com>
2025-10-02 19:02:17 -0500

    hello_world;)

---
+++ print_name/1.0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Description: Asks for the user's name and greets them.
+"""
+#print("Name: Hello World")
+
+strUserInput = input("What is your name?\n->")
+#strUserInput = "Python Learner"  
+
+print(f"Welcome to the Course, {strUserInput}!")
